211496eb4fc8d42bb0462ffd10ff79e73b71b7f6
Eimthlet <mwalwandajonathan24.com>
2025-06-02 18:27:39 +0200

    new deletions

--- front-main/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Build script that ignores TypeScript errors
-export TSC_COMPILE_ON_ERROR=true
-export DISABLE_ESLINT_PLUGIN=true
-export SKIP_PREFLIGHT_CHECK=true
-npm run build
--- front-main/src/polyfills.js
+++
@@ -1,2 +0,0 @@
-// This file provides polyfills for Node.js modules in the browser environment
-// Empty module for crypto - we're using our own implementation
